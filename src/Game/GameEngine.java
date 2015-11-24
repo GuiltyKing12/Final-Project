7a5ddc3f966868a34aaf1e7071e8ce979f513044
GuiltyKing12 <bher@mines.edu>
2015-11-24 12:11:14 -0700

    Added tests to GameEngine (got rid of testTimer for consideration of Lives instead, Added some new functions to GameBoard and GameEngine for testing

--- src/Game/GameEngine.java
+++ src/Game/GameEngine.java
@@ -1,5 +1,27 @@
 package Game;
 
 public class GameEngine {
-
+	private GameBoard board;
+	//private GameDisplay display;
+	
+	public GameEngine() {
+		board = new GameBoard();
+		//display = new GameDisplay();
+		
+		board.initialize();
+		//display.setup();
+	}
+	
+	public GameBoard getBoard() {
+		return board;
+	}
+	
+	public void advanceNextLevel() {
+		
+	}
+	
+	/*public GameDisplay getDisplay() {
+	 * 
+	 * }
+	 */
 }
