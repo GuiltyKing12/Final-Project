4a9c9b24a07ecd42c98fecedf6da59d5e4de25e8
GuiltyKing12 <bher@mines.edu>
2015-11-24 11:10:23 -0700

    Added components to Fraction class

--- src/Game/Fraction.java
+++ src/Game/Fraction.java
@@ -1,5 +1,31 @@
 package Game;
 
 public class Fraction {
+	int numerator;
+	int denominator;
+	
+	public Fraction(int numerator, int denominator) {
+		this.numerator = numerator;
+		this.denominator = denominator;
+	}
+	
+	public int getNumerator() {
+		//return this.numerator;
+		return 0;
+	}
+	
+	public int getDenominator() {
+		//return this.denominator;
+		return 0;
+	}
+	
+	public double getValue() {
+		//return ((double) numerator / (double) denominator);
+		return 0;
+	}
 
+	@Override
+	public String toString() {
+		return (numerator + " / " + denominator );
+	}
 }
