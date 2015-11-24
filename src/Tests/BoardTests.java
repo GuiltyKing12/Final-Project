d345cba965ecc433bcfd88286bdc27ff6e6f31fd
John Spielvogel <jspielvo@mymail.mines.edu>
2015-11-24 08:18:06 -0700

    CHANGES: 	- Added all class stubs (based off of UML diagram) 	- Added stubs for Test Classes (based off of Project Description)
    TODO:
    	- Add failing tests

---
+++ src/Tests/BoardTests.java
@@ -0,0 +1,19 @@
+package Tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class BoardTests {
+	@BeforeClass
+	public void setUp() {
+		
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
