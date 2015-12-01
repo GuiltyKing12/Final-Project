ed062b220c6373f1edc98f2a2afe9c3efdef7239
GuiltyKing12 <bher@mines.edu>
2015-11-30 22:46:49 -0700

    Added some code in order to read and configure a board from csv file

--- src/Game/GameBoard.java
+++ src/Game/GameBoard.java
@@ -1,14 +1,25 @@
 package Game;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Random;
 
+import com.braul.clue.game.BadConfigFormatException;
+import com.braul.clue.game.BoardCell;
+import com.braul.clue.game.DoorDirection;
 import com.sun.javafx.geom.Vec2d;
 
 public class GameBoard {
 	private Vec2d dimensions;
 	private ArrayList<String> boardConfigFile;
 	private BoardCell[][] board;
+	private Map<Character, String> legend;
 	private List<BoardCell> solutionCells;
 	private Player player;
 	
@@ -61,6 +72,46 @@ public class GameBoard {
 	public void startLevel() {
 		// will draw the board
 		// uses the boardConfigFile arrayList -> randomly choose one
+		Random rand = new Random();
+		initializeLevel(boardConfigFile.get(rand.nextInt(3)));
+		
+	}
+	
+	public void initializeLevel(String configFile) {
+		board = new BoardCell[50][50];
+		legend = new HashMap<Character, String>;
+		try {
+			FileReader filo = new FileReader(configFile);
+			BufferedReader br = new BufferedReader(filo);
+			String line = "";
+			int rowCounter = 0;
+			int columnCheck = -1;
+			while((line = br.readLine()) != null){
+				String[] parts = line.split(",");
+				if(columnCheck != -1){
+					if(columnCheck != parts.length){
+						br.close();
+					}
+				}
+				columnCheck = parts.length;
+				for(int i = 0; i < parts.length; i++){
+					char c = parts[i].charAt(0);
+					if(!legend.containsKey(c)){
+						br.close();
+					}
+				}
+				rowCounter++;
+			}
+			dimensions.x = rowCounter;
+			dimensions.y = columnCheck;
+			br.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (NullPointerException e){
+			e.printStackTrace();
+		}
 	}
 	
 	// These are methods to use for the tests
