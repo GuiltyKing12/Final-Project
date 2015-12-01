495e71372f61d46e54c4efc18be91d37cbd652cc
Junquan Lin & Thi La <julin@mines.edu>
2015-12-01 12:10:29 -0700

    upadted questionBank and Fraction classes, need to generate questions and solutions still.

--- src/Game/Fraction.java
+++ src/Game/Fraction.java
@@ -3,25 +3,27 @@ package Game;
 public class Fraction {
 	int numerator;
 	int denominator;
+	double value;
 	
 	public Fraction(int numerator, int denominator) {
 		this.numerator = numerator;
 		this.denominator = denominator;
+		value = (double)numerator/denominator;
 	}
 	
 	public int getNumerator() {
 		//return this.numerator;
-		return 0;
+		return numerator;
 	}
 	
 	public int getDenominator() {
 		//return this.denominator;
-		return 0;
+		return denominator;
 	}
 	
 	public double getValue() {
-		//return ((double) numerator / (double) denominator);
-		return 0;
+		return value;
+		
 	}
 
 	@Override
--- src/Game/QuestionBank.java
+++ src/Game/QuestionBank.java
@@ -8,23 +8,60 @@ public class QuestionBank {
 	// Not sure if we want a string solution, might be better for Fraction solution
 	// We might be able to do map in this case
 
-	private ArrayList<String> question;
-	private ArrayList<String> solution;
+	private ArrayList<Fraction> question;
+	private ArrayList<Fraction> solution;
 	
 	public QuestionBank() {
-		question.add("Test");
-		solution.add("test");
+		Fraction test = new Fraction (1,1);
+		question.add(test);
+		solution.add(test);
 	}
 	
 	public String getQuestion() {
-		return question.get(0);
+		return question.get(0).toString();
 	}
 	
 	public String getSoluton() {
-		return solution.get(0);
+		return solution.get(0).toString();
 	}
 
 	public boolean evaluateGuess() {
 		return false;
 	}
+	
+	public void generateQuestions(){
+		question.clear();
+		solution.clear();
+		//TODO: call solution and add in to arraylist
+	}
+	
+	public Fraction addSolution(int numerator1,int denominator1,int numerator2,int denominator2){
+		
+		int newNumerator;
+		int newDenominator;
+		newNumerator=numerator1*denominator2+denominator1*numerator2;
+		newDenominator=denominator1*denominator2;
+		Fraction temp = new Fraction (newNumerator,newDenominator);
+		return temp;
+	}
+	
+	public Fraction minusSolution(int numerator1,int denominator1,int numerator2,int denominator2){
+		int newNumerator;
+		int newDenominator;
+		newNumerator=numerator1*denominator2-denominator1*numerator2;
+		newDenominator=denominator1*denominator2;
+		Fraction temp = new Fraction (newNumerator,newDenominator);
+		return temp;
+	}
+	
+	public Fraction multiplySolution(int numerator1,int denominator1,int oneInt){
+		int newNumerator;
+		
+		newNumerator=numerator1*oneInt;
+		
+		Fraction temp = new Fraction (newNumerator,denominator1);
+		return temp;
+	}
+	
+	//no need for divisionSolution
 }
